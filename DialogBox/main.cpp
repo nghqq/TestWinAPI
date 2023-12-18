a22169b430f8bce07764b842820be150153fea5a
- <-@DESKTOP-5LUH4K3>
2023-12-18 12:51:57 +0300

    DialogBox in progress

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -2,7 +2,8 @@
 #include"resource.h"
 
 
-
+CONST CHAR g_sz_LOGIN_INVITE[] = "Введите имя пользователя";
+CONST CHAR g_sz_PASSWORD_INVITE[] = "Введите пароль";
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //прототип процедуры окна
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -27,8 +28,47 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;			// Эта секция отрабатывает при запуске окна один раз.
 	case WM_COMMAND:
 		// Здесь обрабатываются команды нажатия кнопок, сочетания клавиш и тд.
-		switch (LOWORD(wParam)) 
+		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+		{
+			CHAR sz_buffer[MAX_PATH] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			SendMessage(hEditLogin, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+
+			//EN_ - Edit Notification
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_LOGIN_INVITE) == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam) == EN_KILLFOCUS && strlen(sz_buffer) == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITE);
+		}
+		break;
+
+		case IDC_EDIT_PASSWORD:
+		{
+			CHAR sz_buffer[MAX_PATH] = {};
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditPassword, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+
+		
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_PASSWORD_INVITE) == 0)
+				SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam) == EN_KILLFOCUS && strlen(sz_buffer) == 0)
+				SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)g_sz_PASSWORD_INVITE);
+		}
+		break;
+
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer); //WM_GETTEXT берет текст
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer); // WM_GETTEXT вставляет текст
+			//SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer); // Копирует 
+		}
+		break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK", "info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
