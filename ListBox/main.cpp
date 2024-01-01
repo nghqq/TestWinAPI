ded9a8210e520bb2b8ad189ac73fcc1101ffa900
- <-@DESKTOP-5LUH4K3>
2024-01-01 10:25:56 +0300

    List Done

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -7,6 +7,7 @@ CONST CHAR* g_sz_VALUES[] = { "This","is","my","first","List","Box" };
 
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //процедура окна
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow) 
@@ -26,36 +27,84 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
+
 		HWND hList = GetDlgItem(hwnd, IDC_LIST);
 		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
 		{
 			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
 		}
-		SendMessage(hList, LB_SETCURSEL, 0, 0);
+		SendMessage(hList, LB_SETCURSEL, 0, 0); // Сразу же выбирает нулевой элемент  ComboBox
+
 		break;
 	}
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_ADD:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_OTHER_DIALOG), hwnd, (DLGPROC)DlgProcAddItem, 0); break;
+		case IDC_DELETE:
+		{
+			CHAR sz_buffer[FILENAME_MAX]{};
+			CHAR sz_message[FILENAME_MAX]{};
+			HWND hCombo = GetDlgItem(hwnd, IDC_LIST);
+			int i = SendMessage(hCombo, LB_GETCURSEL, 0, 0);
+			SendMessage(hCombo, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			sprintf(sz_message, "Вы действительно хотите удалить элемент №%i со значением \"%s\"?", i, sz_buffer);
+			switch (MessageBox(hwnd, sz_message, "Вопрос", MB_YESNO | MB_ICONQUESTION))
+			{
+			case IDYES:SendMessage(hCombo, LB_DELETESTRING, i, 0);
+			case IDNO:break;
+			}
+
+		}
+
 		case IDOK:
 		{
-			HWND hList = GetDlgItem(hwnd, IDC_LIST);
+			HWND hCombo = GetDlgItem(hwnd, IDC_LIST);
 			CHAR sz_buffer[MAX_PATH]{};
 			CHAR sz_message[MAX_PATH]{};
-			int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
-			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			int i = SendMessage(hCombo, LB_GETCURSEL, 0, 0); // Получаем  номер выбранного элемента ComboBox
+			SendMessage(hCombo, LB_GETTEXT, i, (LPARAM)sz_buffer);
 			sprintf(sz_message, "Вы выбрали пункт %i со значением \"%s\".", i, sz_buffer);
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+
 		}
+		break;
 		case IDCANCEL:EndDialog(hwnd, 0);
-
-		//case IDSORT:
-
 			break;
-		case WM_CLOSE:EndDialog(hwnd, 0);
 		}
-		return FALSE;
+		break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
 	}
+	return FALSE;
+}
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_NEW_ITEM));
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CHAR sz_buffer[FILENAME_MAX] = {};
+			HWND hEditAddItem = GetDlgItem(hwnd, IDC_EDIT_NEW_ITEM);
+			SendMessage(hEditAddItem, WM_GETTEXT, FILENAME_MAX, (LPARAM)sz_buffer);
+			HWND hParent = GetParent(hwnd);
+			HWND hCombo = GetDlgItem(hParent, IDC_LIST);
+			//SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			if (SendMessage(hCombo, LB_FINDSTRINGEXACT, -1, (LPARAM)sz_buffer) == LB_ERR)
+				SendMessage(hCombo, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+		}
+		case IDCANCEL:EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
 }
 
 
--- ListBox/resource.h
+++ ListBox/resource.h
@@ -4,17 +4,22 @@
 //
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       103
+#define IDD_OTHER_DIALOG                104
 #define IDC_LIST                        1002
-#define IDC_BUTTON_SORT                 1003
 #define IDSORT                          1003
+#define IDC_ADD                         1004
+#define IDC_DELETE                      1005
+#define IDC_OK                          1007
+#define IDC_EDIT_NEW                    1009
+#define IDC_EDIT_NEW_ITEM               1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
