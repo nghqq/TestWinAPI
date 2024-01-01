514e392bb44d81469132c42ab3d730b4f83f17b4
- <-@DESKTOP-5LUH4K3>
2024-01-01 14:44:49 +0300

    in progress

---
+++ Concatenator/resource1.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDISTUDENT                      103
+#define IDI_ICON_STUDENT                103
+#define IDC_EDIT_FULLNAME               1001
+#define IDC_STATIC_FULLNAME             1002
+#define IDC_EDIT_CHAR                   1003
+#define IDC_STATIC_CHAR                 1004
+#define IDC_CHECK_ATTENTION             1005
+#define IDC_CHECK_GOOD_UNDERSTANDING    1006
+#define IDC_CHECK_GOOD_CODING           1007
+#define IDC_CHECK_NO_HOMEWORK           1008
+#define IDC_CHECK_BAD_ATTENDANCE        1009
+#define IDC_STATIC_POSITIVE             1010
+#define IDC_STATIC_NEGATIVE             1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
